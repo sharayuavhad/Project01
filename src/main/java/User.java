dd92adb255434d34b0737dc71f53a4a21473a476
Sharayu <sharayubavhad@gmail.com>
2025-12-09 19:28:19 +0530

    user class add

---
+++ src/main/java/User.java
@@ -0,0 +1,12 @@
+
+
+public class User{
+    String name;
+    int id;
+    String surname;
+    public User(String name, int id, String surname) {
+        this.name = name;
+        this.id = id;
+        this.surname = surname;
+    }
+}
